d9d8453e5881fbbd37832f2223f26e87ea124bf0
Bega007 <begajan007@gmail.com>
2024-01-16 00:05:20 +0500

    v 1.0.0

---
+++ lib/simple_bloc_observer.dart
@@ -0,0 +1,22 @@
+// ignore_for_file: avoid_print
+
+import 'package:bloc/bloc.dart';
+
+class SimpleBlockObserver extends BlocObserver {
+  const SimpleBlockObserver();
+
+  @override
+  void onTransition(
+    Bloc<dynamic, dynamic> bloc,
+    Transition<dynamic, dynamic> transition,
+  ) {
+    super.onTransition(bloc, transition);
+    print(transition);
+  }
+
+  @override
+  void onError(BlocBase<dynamic> bloc, Object error, StackTrace stackTrace) {
+    print(error);
+    super.onError(bloc, error, stackTrace);
+  }
+}
